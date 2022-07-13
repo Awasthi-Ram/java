8c9ab5cd0b8c3dae7507f2a474f63b2646f765a4
ramawasthi300 <ramawasthi300@gmail.com>
2022-07-13 11:19:44 +0530

    exception commit

---
+++ exception.java
@@ -0,0 +1,13 @@
+public class exception {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 0;
+        try{
+        int c =a/b;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+        System.out.println("hello");
+    }
+}
---
+++ javaclg/src/javaclg/ram.java
@@ -0,0 +1,5 @@
+package javaclg;
+
+public class ram {
+
+}
